ac230d312e72e28e6036a4923b13d6b0791c8b89
Caolan McMahon <caolan@caolanmcmahon.com>
2012-10-04 09:07:19 -0700

    added validator function and tests

--- test.js
+++ test.js
@@ -97,3 +97,194 @@ exports['do not allow extra properties'] = function (test) {
     ]);
     test.done();
 };
+
+exports['validator - undefined'] = function (test) {
+    var schema = {foo: v.undefined};
+    test.equal(v.validate(schema, {}).length, 0);
+    test.equal(v.validate(schema, {foo: null}).length, 1);
+    test.equal(v.validate(schema, {foo: 123}).length, 1);
+    test.done();
+};
+
+exports['validator - string'] = function (test) {
+    var schema = {foo: v.string};
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 0);
+    test.equal(v.validate(schema, {foo: null}).length, 1);
+    test.equal(v.validate(schema, {foo: 123}).length, 1);
+    test.done();
+};
+
+exports['validator - number'] = function (test) {
+    var schema = {foo: v.number};
+    test.equal(v.validate(schema, {foo: 123}).length, 0);
+    test.equal(v.validate(schema, {foo: null}).length, 1);
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 1);
+    test.done();
+};
+
+exports['validator - array'] = function (test) {
+    var schema = {foo: v.array};
+    test.equal(v.validate(schema, {foo: [1,2,3]}).length, 0);
+    test.equal(v.validate(schema, {foo: {}}).length, 1);
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 1);
+    test.done();
+};
+
+exports['validator - boolean'] = function (test) {
+    var schema = {foo: v.boolean};
+    test.equal(v.validate(schema, {foo: true}).length, 0);
+    test.equal(v.validate(schema, {foo: false}).length, 0);
+    test.equal(v.validate(schema, {foo: {}}).length, 1);
+    test.equal(v.validate(schema, {foo: 123}).length, 1);
+    test.done();
+};
+
+exports['validator - object'] = function (test) {
+    var schema = {foo: v.object};
+    test.equal(v.validate(schema, {foo: {bar: 'asdf'}}).length, 0);
+    test.equal(v.validate(schema, {foo: {}}).length, 0);
+    test.equal(v.validate(schema, {foo: [1,2,3]}).length, 1);
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 1);
+    test.done();
+};
+
+exports['validator - min'] = function (test) {
+    var schema = {foo: v.min(5)};
+    test.equal(v.validate(schema, {foo: 5}).length, 0);
+    test.equal(v.validate(schema, {foo: 10}).length, 0);
+    test.equal(v.validate(schema, {foo: 0}).length, 1);
+    test.equal(v.validate(schema, {foo: 4.99}).length, 1);
+    test.done();
+};
+
+exports['validator - max'] = function (test) {
+    var schema = {foo: v.max(5)};
+    test.equal(v.validate(schema, {foo: 0}).length, 0);
+    test.equal(v.validate(schema, {foo: 4.99}).length, 0);
+    test.equal(v.validate(schema, {foo: 5}).length, 0);
+    test.equal(v.validate(schema, {foo: 10}).length, 1);
+    test.done();
+};
+
+exports['validator - range'] = function (test) {
+    var schema = {foo: v.range(2, 5)};
+    test.equal(v.validate(schema, {foo: 0}).length, 1);
+    test.equal(v.validate(schema, {foo: 3.33}).length, 0);
+    test.equal(v.validate(schema, {foo: 2}).length, 0);
+    test.equal(v.validate(schema, {foo: 5}).length, 0);
+    test.equal(v.validate(schema, {foo: 6}).length, 1);
+    test.done();
+};
+
+exports['validator - maxLength'] = function (test) {
+    var schema = {foo: v.maxLength(5)};
+    test.equal(v.validate(schema, {foo: [1,2,3]}).length, 0);
+    test.equal(v.validate(schema, {foo: [1,2,3,4,5,6,7]}).length, 1);
+    test.equal(v.validate(schema, {foo: 'abcde'}).length, 0);
+    test.equal(v.validate(schema, {foo: 'abcdef'}).length, 1);
+    test.equal(v.validate(schema, {foo: []}).length, 0);
+    test.equal(v.validate(schema, {foo: ''}).length, 0);
+    test.done();
+};
+
+exports['validator - minLength'] = function (test) {
+    var schema = {foo: v.minLength(5)};
+    test.equal(v.validate(schema, {foo: [1,2,3]}).length, 1);
+    test.equal(v.validate(schema, {foo: [1,2,3,4,5,6,7]}).length, 0);
+    test.equal(v.validate(schema, {foo: 'abcde'}).length, 0);
+    test.equal(v.validate(schema, {foo: 'abcdef'}).length, 0);
+    test.equal(v.validate(schema, {foo: []}).length, 1);
+    test.equal(v.validate(schema, {foo: ''}).length, 1);
+    test.done();
+};
+
+exports['validator - rangeLength'] = function (test) {
+    var schema = {foo: v.rangeLength(2, 5)};
+    test.equal(v.validate(schema, {foo: [1]}).length, 1);
+    test.equal(v.validate(schema, {foo: [1,2]}).length, 0);
+    test.equal(v.validate(schema, {foo: [1,2,3,4,5]}).length, 0);
+    test.equal(v.validate(schema, {foo: [1,2,3,4,5,6]}).length, 1);
+    test.equal(v.validate(schema, {foo: 'a'}).length, 1);
+    test.equal(v.validate(schema, {foo: 'ab'}).length, 0);
+    test.equal(v.validate(schema, {foo: 'abcdef'}).length, 1);
+    test.equal(v.validate(schema, {foo: []}).length, 1);
+    test.equal(v.validate(schema, {foo: ''}).length, 1);
+    test.done();
+};
+
+exports['validator - regexp'] = function (test) {
+    var schema = {foo: v.regexp(/^testing$/)};
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 1);
+    test.equal(v.validate(schema, {foo: 'testing'}).length, 0);
+    test.done();
+};
+
+exports['validator - regexp with string'] = function (test) {
+    var schema = {foo: v.regexp('^testing$')};
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 1);
+    test.equal(v.validate(schema, {foo: 'testing'}).length, 0);
+    test.done();
+};
+
+exports['validator - email'] = function (test) {
+    var schema = {foo: v.email};
+    test.equal(v.validate(schema, {foo: 'user@example.com'}).length, 0);
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 1);
+    test.done();
+};
+
+exports['validator - url'] = function (test) {
+    var schema = {foo: v.url};
+    test.equal(v.validate(schema, {foo: 'http://example.com'}).length, 0);
+    test.equal(v.validate(schema, {foo: 'https://example.com'}).length, 0);
+    test.equal(v.validate(schema, {foo: 'asdf'}).length, 1);
+    test.done();
+};
+
+exports['all'] = function (test) {
+    var vpass = function () { return []; };
+    var vfail1 = function () { return [{error: 'failure1'}]; };
+    var vfail2 = function () { return [{error: 'failure2'}]; };
+
+    var s1 = {foo: v.all(vpass, vpass)};
+    test.equal(v.validate(s1, {foo: null}).length, 0);
+
+    var s2 = {foo: v.all(vpass, vfail1)};
+    test.same(v.validate(s2, {foo: null}), [
+        {error: 'failure1', errors: [{error: 'failure1'}], path: ['foo']}
+    ]);
+
+    var s3 = {foo: v.all(vfail1, vfail2)};
+    test.same(v.validate(s3, {foo: null}), [
+        {
+            error: 'failure1 AND failure2',
+            errors: [{error: 'failure1'}, {error: 'failure2'}],
+            path: ['foo']
+        }
+    ]);
+
+    test.done();
+};
+
+exports['any'] = function (test) {
+    var vpass = function () { return []; };
+    var vfail1 = function () { return [{error: 'failure1'}]; };
+    var vfail2 = function () { return [{error: 'failure2'}]; };
+
+    var s1 = {foo: v.any(vpass, vpass)};
+    test.equal(v.validate(s1, {foo: null}).length, 0);
+
+    var s2 = {foo: v.any(vpass, vfail1)};
+    test.same(v.validate(s2, {foo: null}).length, 0);
+
+    var s3 = {foo: v.any(vfail1, vfail2)};
+    test.same(v.validate(s3, {foo: null}), [
+        {
+            error: 'failure1 OR failure2',
+            errors: [{error: 'failure1'}, {error: 'failure2'}],
+            path: ['foo']
+        }
+    ]);
+
+    test.done();
+};
--- validate.js
+++ validate.js
@@ -44,6 +44,36 @@ exports.string = function (doc, value) {
     return [];
 };
 
+exports.number = function (doc, value) {
+    if (toString.call(value) !== '[object Number]') {
+        return [{error: 'Expected number'}];
+    }
+    return [];
+};
+
+exports.array = function (doc, value) {
+    if (toString.call(value) !== '[object Array]') {
+        return [{error: 'Expected array'}];
+    }
+    return [];
+};
+
+exports.boolean = function (doc, value) {
+    if (value === true ||
+        value === false ||
+        toString.call(value) === '[object Boolean]') {
+        return [];
+    }
+    return [{error: 'Expected boolean'}];
+};
+
+exports.object = function (doc, value) {
+    if (value !== Object(value) || toString.call(value) === '[object Array]') {
+        return [{error: 'Expected object'}];
+    }
+    return [];
+};
+
 exports.min = function (min) {
     return function (doc, value) {
         if (value < min) {
@@ -77,6 +107,60 @@ exports.range = function (min, max) {
     };
 };
 
+exports.minLength = function (val) {
+    return function (doc, value) {
+        if (value.length < val) {
+            return [
+                {error: 'Please enter at least ' + val + ' characters'}
+            ];
+        }
+        return [];
+    };
+};
+
+exports.maxLength = function (val) {
+    return function (doc, value) {
+        if (value.length > val) {
+            return [
+                {error: 'Please enter no more than ' + val + ' characters'}
+            ];
+        }
+        return [];
+    };
+};
+
+exports.rangeLength = function (min, max) {
+    return function (doc, value) {
+        if (value.length < min || value.length > max) {
+            return [
+                {error: 'Please enter a value between ' + min + ' and ' + max +
+                        ' characters long'}
+            ];
+        }
+        return [];
+    };
+};
+
+exports.regexp = function (re, message) {
+    re = (typeof re === 'string') ? new RegExp(re): re;
+    return function (doc, value) {
+        if (!re.test(value)) {
+            return [
+                {error: message || 'Invalid format'}
+            ];
+        }
+        return [];
+    };
+};
+
+// regular expression by Scott Gonzalez:
+// http://projects.scottsplayground.com/email_address_validation/
+exports.email = exports.regexp(new RegExp("^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?$", "i"), 'Please enter a valid email address');
+
+// regular expression by Scott Gonzalez:
+// http://projects.scottsplayground.com/iri/
+exports.url = exports.regexp(new RegExp("^(https?|ftp):\\/\\/(((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:)*@)?(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))|((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?)(:\\d*)?)(\\/((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)+(\\/(([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)*)*)?)?(\\?((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|[\\uE000-\\uF8FF]|\\/|\\?)*)?(\\#((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|\\/|\\?)*)?$", "i"), 'Please enter a valid URL');
+
 exports.any = function (/* args... */) {
     var args = slice.call(arguments);
 
@@ -91,6 +175,24 @@ exports.any = function (/* args... */) {
         }
         var msgs = map(errs, function (e) { return e.error; });
         return [{error: msgs.join(' OR '), errors: errs}];
+    };
+};
+
+exports.all = function (/* args... */) {
+    var args = slice.call(arguments);
+
+    return function (doc, value) {
+        var errs = [];
+        for (var i = 0, l = args.length; i < l; i++) {
+            errs = errs.concat(args[i](doc, value));
+        }
+        if (!errs.length) {
+            return [];
+        }
+        else {
+            var msgs = map(errs, function (e) { return e.error; });
+            return [{error: msgs.join(' AND '), errors: errs}];
+        }
     };
 };
 
